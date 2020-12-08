977c2192fb023b1fbd5705d039a58341f37d4b01
Tanya <peachpeachttt@gmail.com>
2020-12-08 12:45:59 +0300

    Синхронизация ветки с master

--- src/Pages/Auth/Auth.tsx
+++ src/Pages/Auth/Auth.tsx
@@ -80,3 +80,96 @@ const Auth: FC = () => {
 };
 
 export default Auth;
+
+// import React, { useEffect, useState } from "react";
+// import "./App.css";
+// import styled from "styled-components";
+// import firebase from "firebase/app";
+// import "firebase/database";
+// import "firebase/auth";
+
+// const Form = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   width: 600px;
+//   margin: 60px auto;
+// `;
+
+// const App: React.FC = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [hasAccount, setHasAccount] = useState(false);
+//   const [name, setName] = useState("");
+//   const [key, setKey] = useState("");
+//   const [keyValue, setKeyValue] = useState("");
+
+//   useEffect(() => {
+//     const db = firebase.database();
+//     // const name = db.ref("name"); //получаем имя из БД и заносим его в стейт
+//     // name.on("value", (elem) => setName(elem.val()));
+//     // console.log(name);
+//   }, []);
+
+//   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const { value, id } = e.target;
+
+//     if (id === "email") {
+//       setEmail(value);
+//     }
+//     if (id === "password") {
+//       setPassword(value);
+//     }
+//     if (id === "key") {
+//       setKey(value);
+//     }
+//     if (id === "keyValue") {
+//       setKeyValue(value);
+//     }
+//   };
+
+//   const createAccount = (): void => {
+//     // firebase
+//     //   .auth()
+//     //   .createUserWithEmailAndPassword(email, password) //создание аккаунта
+//     //   .catch(({ message }) => console.error(message));
+
+//     firebase
+//       .auth()
+//       .signInWithEmailAndPassword(email, password) //войти с помощью почты и пароля
+//       .then(() => setHasAccount(true))
+//       .catch(({ message }) => console.error(message));
+//   };
+
+//   const sendData = () => {
+//     const db = firebase.database();
+//     db.ref(key).push(keyValue);
+//     alert("записалось");
+//   };
+
+//   if (hasAccount) {
+//     return (
+//       <div>
+//         <input type="text" placeholder="key" id="key" onChange={handleChange} />
+//         <input
+//           type="text"
+//           placeholder="value"
+//           id="keyValue"
+//           onChange={handleChange}
+//         />
+//         <button type="button" onClick={sendData}>
+//           Отравить
+//         </button>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <Form>
+//       <input type="text" id="email" onChange={handleChange} />
+//       <input type="password" id="password" onChange={handleChange} />
+//       <button type="button" onClick={createAccount}>
+//         Войти
+//       </button>
+//     </Form>
+//   );
+// };
