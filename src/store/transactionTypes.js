7ce2f8146432e26aeaa647fa0dec31e6ffb28ab3
Helen Kim <62159547+Lena-Ki@users.noreply.github.com>
2020-12-16 12:25:46 +0300

    Delete transactionTypes.js

--- src/store/transactionTypes.js
+++
@@ -1,8 +0,0 @@
-const types = [
-  { type: "transaction", name: "Переводы" },
-  { type: "income", name: "Пополнения" },
-  { type: "service", name: "Товары и услуги" },
-  { type: "entertainment", name: "Развлечения" },
-];
-
-export { types };
