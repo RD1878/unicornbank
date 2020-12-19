b7c93c0a51a10c11f607d3b374c7a6480d35c506
Tanya <peachpeachttt@gmail.com>
2020-12-19 15:41:07 +0300

    Стилизация sidebar с помощью classes api

--- src/organisms/Sidebar.tsx
+++ src/organisms/Sidebar.tsx
@@ -1,9 +1,10 @@
 import React, { FC, useState } from "react";
-import styled from "styled-components";
-import { withTheme } from "@material-ui/core/styles";
+import clsx from "clsx";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
+import { withTheme } from "@material-ui/core/styles";
 import { Box, Avatar } from "@material-ui/core";
 import { Link } from "@material-ui/core";
 import CreateRoundedIcon from "@material-ui/icons/CreateRounded";
@@ -13,21 +14,16 @@ import FormatIndentDecreaseRoundedIcon from "@material-ui/icons/FormatIndentDecr
 import FormatIndentIncreaseRoundedIcon from "@material-ui/icons/FormatIndentIncreaseRounded";
 import Tooltip from "@material-ui/core/Tooltip";
 import Grid from "@material-ui/core/Grid";
+import styled from "styled-components";
 import { routes } from ".././routes";
-
 interface IWithOpen {
   open: boolean;
 }
-
-interface IStyledDrawer extends IWithOpen {
-  width: number;
-}
-
 interface ISidebar {
   fullName: string;
 }
 
-const OPENED_DRAWER_WIDTH = 350;
+// const OPENED_DRAWER_WIDTH = 350;
 
 const CARDS = [
   {
@@ -48,43 +44,55 @@ const CARDS = [
   },
 ];
 
-const StyledDrawer = withTheme(styled(Drawer)<IStyledDrawer>`
-  & > div {
-    position: relative;
-    width: ${(props) => props.width}px;
-    background-color: ${(props) => props.theme.palette.primary.main};
-    transition: all 0.2s ease-in-out;
-    overflow-y: unset;
-    overflow-x: ${(props) => (props.open ? "unset" : "hidden")};
-    width: ${(props) => (props.open ? props.width : props.theme.spacing(15))}px;
-    transition: ${(props) =>
-      props.theme.transitions.create("width", {
-        easing: props.theme.transitions.easing.sharp,
-        duration: props.open
-          ? props.theme.transitions.duration.enteringScreen
-          : props.theme.transitions.duration.leavingScreen,
-      })};
+const drawerWidth = 350;
 
-    & > div {
-      position: sticky;
-      top: 0;
-    }
-  }
-`);
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    hide: {
+      display: "none",
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    paper: {
+      position: "relative",
+      overflowY: "unset",
+    },
+    drawerOpen: {
+      width: drawerWidth,
+      backgroundColor: theme.palette.primary.main,
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerClose: {
+      backgroundColor: theme.palette.primary.main,
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      overflowX: "hidden",
+      width: theme.spacing(15),
+    },
+  })
+);
 
 const StyledProfileInfo = withTheme(styled(Box)<IWithOpen>`
-  opacity: ${(props) => (props.open ? 1 : 0)};
-  transform: ${(props) => (props.open ? "scale(1)" : "scale(0)")};
-  height: ${(props) => (props.open ? "auto" : 0)};
-  pointer-events: ${(props) => (props.open ? "all" : "none")};
-  transition: ${(props) =>
-    props.theme.transitions.create("all", {
-      easing: props.theme.transitions.easing.sharp,
-      duration: props.open
-        ? props.theme.transitions.duration.enteringScreen
-        : props.theme.transitions.duration.leavingScreen,
-    })};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `);
+
+const StyledWrap = styled(Box)`
+  position: sticky;
+  top: 0;
+`;
 
 const StyledAvatar = withTheme(styled(Avatar)`
   width: 100px;
@@ -96,6 +104,7 @@ const StyledLink = withTheme(styled(Link)`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-top: 10px;
 
   p {
     margin-left: 10px;
@@ -103,6 +112,7 @@ const StyledLink = withTheme(styled(Link)`
 `);
 
 const Sidebar: FC<ISidebar> = ({ fullName }) => {
+  const classes = useStyles();
   const [open, setOpen] = useState(true);
 
   const handleDrawerCollapse = () => {
@@ -110,24 +120,36 @@ const Sidebar: FC<ISidebar> = ({ fullName }) => {
   };
 
   return (
-    <StyledDrawer width={OPENED_DRAWER_WIDTH} variant="permanent" open={open}>
-      <Box>
-        <StyledProfileInfo open={open}>
-          <Box m={3}>
-            <Grid container direction="column" alignItems="center">
+    <Drawer
+      variant="permanent"
+      className={clsx(classes.drawer, {
+        [classes.drawerOpen]: open,
+        [classes.drawerClose]: !open,
+      })}
+      classes={{
+        paper: clsx(classes.paper, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        }),
+      }}
+    >
+      <StyledWrap>
+        {open && (
+          <StyledProfileInfo m={3}>
+            <Grid container justify="center" alignItems="center">
               <StyledAvatar sizes="large">H</StyledAvatar>
               <Typography variant="h2" color="textPrimary" align="center">
                 {fullName}
               </Typography>
             </Grid>
-          </Box>
-          <StyledLink href={routes.main}>
-            <CreateRoundedIcon color="action" />
-            <Typography variant="body1" color="textSecondary" align="center">
-              Редактировать профиль
-            </Typography>
-          </StyledLink>
-        </StyledProfileInfo>
+            <StyledLink href={routes.main}>
+              <CreateRoundedIcon color="action" />
+              <Typography variant="body1" color="textSecondary" align="center">
+                Редактировать профиль
+              </Typography>
+            </StyledLink>
+          </StyledProfileInfo>
+        )}
         <Box mt={5}>
           <List>
             {CARDS.map((card) => (
@@ -146,8 +168,8 @@ const Sidebar: FC<ISidebar> = ({ fullName }) => {
             </IconButton>
           </Tooltip>
         </Grid>
-      </Box>
-    </StyledDrawer>
+      </StyledWrap>
+    </Drawer>
   );
 };
 
