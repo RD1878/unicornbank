f37580aba913d68ecf0866933410e39335dac50d
Pavel <khokhlovpavel1986@yandex.ru>
2021-02-17 23:18:37 +0300

    Добавлены сценарии по тестам на авторизацию и регистрацию

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -33,4 +33,58 @@ describe("Login page ", () => {
       }
     );
   });
+  it("This happens when you enter incorrect email", () => {
+    cy.fixture("unicornTestLogin").then(
+      ({ main_url, incorrectEmail, password, incorrectEmailErrorMessage }) => {
+        cy.log("Login page");
+        cy.visit(main_url);
+        cy.log("entering email");
+        cy.get(`[data-test-id=${ELEMENT.loginEmail}]`).type(incorrectEmail);
+        cy.log("entering password");
+        cy.get(`[data-test-id=${ELEMENT.password}]`).type(password);
+        cy.log("clicking logging button");
+        cy.get(`[data-test-id=${ELEMENT.loginButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          incorrectEmailErrorMessage
+        );
+      }
+    );
+  });
+  it("This happens when you enter without email", () => {
+    cy.fixture("unicornTestLogin").then(
+      ({ main_url, password, withoutEmailErrorMessage }) => {
+        cy.log("Login page");
+        cy.visit(main_url);
+        cy.log("entering email");
+        cy.get(`[data-test-id=${ELEMENT.loginEmail}]`);
+        cy.log("entering password");
+        cy.get(`[data-test-id=${ELEMENT.password}]`).type(password);
+        cy.log("clicking logging button");
+        cy.get(`[data-test-id=${ELEMENT.loginButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          withoutEmailErrorMessage
+        );
+      }
+    );
+  });
+  it("This happens when you enter without password", () => {
+    cy.fixture("unicornTestLogin").then(
+      ({ main_url, email, withoutPasswordErrorMessage }) => {
+        cy.log("Login page");
+        cy.visit(main_url);
+        cy.log("entering email");
+        cy.get(`[data-test-id=${ELEMENT.loginEmail}]`).type(email);
+        cy.log("entering password");
+        cy.get(`[data-test-id=${ELEMENT.password}]`);
+        cy.log("clicking logging button");
+        cy.get(`[data-test-id=${ELEMENT.loginButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          withoutPasswordErrorMessage
+        );
+      }
+    );
+  });
 });
--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -24,16 +24,92 @@ describe("Register page ", () => {
         cy.visit(main_url);
         cy.log("entering email");
         cy.get(`[data-test-id=${ELEMENT.registerEmail}]`).type(email);
-        cy.log("entering shortPassword1");
+        cy.log("entering password1");
         cy.get(`[data-test-id=${ELEMENT.password1}]`).type(shortPassword);
         cy.log("entering password2");
         cy.get(`[data-test-id=${ELEMENT.password2}]`).type(shortPassword);
+        cy.log("clicking registering button");
+        cy.get(`[data-test-id=${ELEMENT.registerButton}]`).click();
         cy.get(".MuiFormHelperText-root").should(
           "contain",
           shortPasswordErrorMessage
         );
-        cy.log("clicking registering button");
+      }
+    );
+  });
+  it("This happens when you enter incorrect email", () => {
+    cy.fixture("unicornTestRegister").then(
+      ({ main_url, incorrectEmail, password, incorrectEmailErrorMessage }) => {
+        cy.log("Register page");
+        cy.visit(main_url);
+        cy.log("entering email");
+        cy.get(`[data-test-id=${ELEMENT.registerEmail}]`).type(incorrectEmail);
+        cy.get(`[data-test-id=${ELEMENT.password1}]`).type(password);
+        cy.log("entering password2");
+        cy.get(`[data-test-id=${ELEMENT.password2}]`).type(password);
+        cy.log("clicking logging button");
         cy.get(`[data-test-id=${ELEMENT.registerButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          incorrectEmailErrorMessage
+        );
+      }
+    );
+  });
+  it("This happens when you enter without email", () => {
+    cy.fixture("unicornTestRegister").then(
+      ({ main_url, password, withoutEmailErrorMessage }) => {
+        cy.log("Register page");
+        cy.visit(main_url);
+        cy.log("entering email");
+        cy.get(`[data-test-id=${ELEMENT.registerEmail}]`);
+        cy.get(`[data-test-id=${ELEMENT.password1}]`).type(password);
+        cy.log("entering password2");
+        cy.get(`[data-test-id=${ELEMENT.password2}]`).type(password);
+        cy.log("clicking logging button");
+        cy.get(`[data-test-id=${ELEMENT.registerButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          withoutEmailErrorMessage
+        );
+      }
+    );
+  });
+  it("This happens when you enter without password1", () => {
+    cy.fixture("unicornTestRegister").then(
+      ({ main_url, password, withoutPasswordErrorMessage, email }) => {
+        cy.log("Register page");
+        cy.visit(main_url);
+        cy.log("entering email");
+        cy.get(`[data-test-id=${ELEMENT.registerEmail}]`).type(email);
+        cy.get(`[data-test-id=${ELEMENT.password1}]`);
+        cy.log("entering password2");
+        cy.get(`[data-test-id=${ELEMENT.password2}]`).type(password);
+        cy.log("clicking logging button");
+        cy.get(`[data-test-id=${ELEMENT.registerButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          withoutPasswordErrorMessage
+        );
+      }
+    );
+  });
+  it("This happens when you enter without password2", () => {
+    cy.fixture("unicornTestRegister").then(
+      ({ main_url, password, repeatPasswordErrorMessage, email }) => {
+        cy.log("Register page");
+        cy.visit(main_url);
+        cy.log("entering email");
+        cy.get(`[data-test-id=${ELEMENT.registerEmail}]`).type(email);
+        cy.get(`[data-test-id=${ELEMENT.password1}]`).type(password);
+        cy.log("entering password2");
+        cy.get(`[data-test-id=${ELEMENT.password2}]`);
+        cy.log("clicking logging button");
+        cy.get(`[data-test-id=${ELEMENT.registerButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          repeatPasswordErrorMessage
+        );
       }
     );
   });
