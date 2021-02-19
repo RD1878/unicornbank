b28a3048dd7ffd4778634d3f115ce28edf0e9f3c
Pavel <khokhlovpavel1986@yandex.ru>
2021-02-19 23:01:38 +0300

    Добавлена проверка placeholder полей

--- cypress/integration/settings.spec.js
+++ cypress/integration/settings.spec.js
@@ -5,15 +5,28 @@ import { ELEMENT } from "../../src/constants";
 describe("Settings page", () => {
   it("This happens when you enter correct data", () => {
     cy.fixture("unicornTestSettings").then(
-      ({ main_url, password, newPassword }) => {
+      ({
+        main_url,
+        password,
+        newPassword,
+        textCurrentPasswordField,
+        textNewPasswordField,
+        textRepeatPasswordField,
+      }) => {
         cy.log("Settings page");
         cy.visit(main_url);
         cy.log("entering current password");
-        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(password);
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`)
+          .should("have.text", textCurrentPasswordField)
+          .type(password);
         cy.log("entering new password");
-        cy.get(`[data-test-id=${ELEMENT.newPassword}]`).type(newPassword);
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`)
+          .should("have.text", textNewPasswordField)
+          .type(newPassword);
         cy.log("entering repeat new password");
-        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`).type(newPassword);
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`)
+          .should("have.text", textRepeatPasswordField)
+          .type(newPassword);
         cy.log("clicking save button");
         cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
       }
@@ -26,15 +39,24 @@ describe("Settings page", () => {
         shortCurrentPasswordErrorMessage,
         shortPassword,
         newPassword,
+        textCurrentPasswordField,
+        textNewPasswordField,
+        textRepeatPasswordField,
       }) => {
         cy.log("Settings page");
         cy.visit(main_url);
         cy.log("entering current password");
-        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(shortPassword);
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`)
+          .should("have.text", textCurrentPasswordField)
+          .type(shortPassword);
         cy.log("entering new password");
-        cy.get(`[data-test-id=${ELEMENT.newPassword}]`).type(newPassword);
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`)
+          .should("have.text", textNewPasswordField)
+          .type(newPassword);
         cy.log("entering repeat new password");
-        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`).type(newPassword);
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`)
+          .should("have.text", textRepeatPasswordField)
+          .type(newPassword);
         cy.log("clicking save button");
         cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
         cy.get(".MuiFormHelperText-root").should(
@@ -46,15 +68,30 @@ describe("Settings page", () => {
   });
   it("This happens when you enter short new password1", () => {
     cy.fixture("unicornTestSettings").then(
-      ({ main_url, withoutPasswordErrorMessage, password, newPassword }) => {
+      ({
+        main_url,
+        withoutPasswordErrorMessage,
+        password,
+        newPassword,
+        textCurrentPasswordField,
+        textNewPasswordField,
+        textRepeatPasswordField,
+      }) => {
         cy.log("Settings page");
         cy.visit(main_url);
         cy.log("entering current password");
-        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(password);
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`)
+          .should("have.text", textCurrentPasswordField)
+          .type(password);
         cy.log("entering new password");
-        cy.get(`[data-test-id=${ELEMENT.newPassword}]`);
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`).should(
+          "have.text",
+          textNewPasswordField
+        );
         cy.log("entering repeat new password");
-        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`).type(newPassword);
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`)
+          .should("have.text", textRepeatPasswordField)
+          .type(newPassword);
         cy.log("clicking save button");
         cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
         cy.get(".MuiFormHelperText-root").should(
@@ -66,15 +103,30 @@ describe("Settings page", () => {
   });
   it("This happens when you enter without new password2", () => {
     cy.fixture("unicornTestSettings").then(
-      ({ main_url, repeatPasswordErrorMessage, password, newPassword }) => {
+      ({
+        main_url,
+        repeatPasswordErrorMessage,
+        password,
+        newPassword,
+        textCurrentPasswordField,
+        textNewPasswordField,
+        textRepeatPasswordField,
+      }) => {
         cy.log("Settings page");
         cy.visit(main_url);
         cy.log("entering current password");
-        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(password);
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`)
+          .should("have.text", textCurrentPasswordField)
+          .type(password);
         cy.log("entering new password");
-        cy.get(`[data-test-id=${ELEMENT.newPassword}]`).type(newPassword);
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`)
+          .should("have.text", textNewPasswordField)
+          .type(newPassword);
         cy.log("entering repeat new password");
-        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`);
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`).should(
+          "have.text",
+          textRepeatPasswordField
+        );
         cy.log("clicking save button");
         cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
         cy.get(".MuiFormHelperText-root").should(
@@ -92,15 +144,24 @@ describe("Settings page", () => {
         password,
         newPassword,
         shortPassword,
+        textCurrentPasswordField,
+        textNewPasswordField,
+        textRepeatPasswordField,
       }) => {
         cy.log("Settings page");
         cy.visit(main_url);
         cy.log("entering current password");
-        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(password);
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`)
+          .should("have.text", textCurrentPasswordField)
+          .type(password);
         cy.log("entering new password");
-        cy.get(`[data-test-id=${ELEMENT.newPassword}]`).type(shortPassword);
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`)
+          .should("have.text", textNewPasswordField)
+          .type(shortPassword);
         cy.log("entering repeat new password");
-        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`).type(newPassword);
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`)
+          .should("have.text", textRepeatPasswordField)
+          .type(newPassword);
         cy.log("clicking save button");
         cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
         cy.get(".MuiFormHelperText-root").should(
@@ -118,17 +179,24 @@ describe("Settings page", () => {
         password,
         newPassword,
         shortPassword,
+        textCurrentPasswordField,
+        textNewPasswordField,
+        textRepeatPasswordField,
       }) => {
         cy.log("Settings page");
         cy.visit(main_url);
         cy.log("entering current password");
-        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(password);
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`)
+          .should("have.text", textCurrentPasswordField)
+          .type(password);
         cy.log("entering new password");
-        cy.get(`[data-test-id=${ELEMENT.newPassword}]`).type(newPassword);
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`)
+          .should("have.text", textNewPasswordField)
+          .type(newPassword);
         cy.log("entering repeat new password");
-        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`).type(
-          shortPassword
-        );
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`)
+          .should("have.text", textRepeatPasswordField)
+          .type(shortPassword);
         cy.log("clicking save button");
         cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
         cy.get(".MuiFormHelperText-root").should(
