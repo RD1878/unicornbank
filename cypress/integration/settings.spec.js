8cd4c3c15b5931bcd9c40f01821e65640b546062
Pavel <khokhlovpavel1986@yandex.ru>
2021-02-19 21:57:56 +0300

    Реализованы дополнительные сценарии тестов для страницы с изменением пароля

--- cypress/integration/settings.spec.js
+++ cypress/integration/settings.spec.js
@@ -19,4 +19,123 @@ describe("Settings page", () => {
       }
     );
   });
+  it("This happens when you enter short current password", () => {
+    cy.fixture("unicornTestSettings").then(
+      ({
+        main_url,
+        shortCurrentPasswordErrorMessage,
+        shortPassword,
+        newPassword,
+      }) => {
+        cy.log("Settings page");
+        cy.visit(main_url);
+        cy.log("entering current password");
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(shortPassword);
+        cy.log("entering new password");
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`).type(newPassword);
+        cy.log("entering repeat new password");
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`).type(newPassword);
+        cy.log("clicking save button");
+        cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          shortCurrentPasswordErrorMessage
+        );
+      }
+    );
+  });
+  it("This happens when you enter short new password1", () => {
+    cy.fixture("unicornTestSettings").then(
+      ({ main_url, withoutPasswordErrorMessage, password, newPassword }) => {
+        cy.log("Settings page");
+        cy.visit(main_url);
+        cy.log("entering current password");
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(password);
+        cy.log("entering new password");
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`);
+        cy.log("entering repeat new password");
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`).type(newPassword);
+        cy.log("clicking save button");
+        cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          withoutPasswordErrorMessage
+        );
+      }
+    );
+  });
+  it("This happens when you enter without new password2", () => {
+    cy.fixture("unicornTestSettings").then(
+      ({ main_url, repeatPasswordErrorMessage, password, newPassword }) => {
+        cy.log("Settings page");
+        cy.visit(main_url);
+        cy.log("entering current password");
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(password);
+        cy.log("entering new password");
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`).type(newPassword);
+        cy.log("entering repeat new password");
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`);
+        cy.log("clicking save button");
+        cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          repeatPasswordErrorMessage
+        );
+      }
+    );
+  });
+  it("This happens when you enter short new password1", () => {
+    cy.fixture("unicornTestSettings").then(
+      ({
+        main_url,
+        shortPasswordErrorMessage,
+        password,
+        newPassword,
+        shortPassword,
+      }) => {
+        cy.log("Settings page");
+        cy.visit(main_url);
+        cy.log("entering current password");
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(password);
+        cy.log("entering new password");
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`).type(shortPassword);
+        cy.log("entering repeat new password");
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`).type(newPassword);
+        cy.log("clicking save button");
+        cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          shortPasswordErrorMessage
+        );
+      }
+    );
+  });
+  it("This happens when you enter short new password2", () => {
+    cy.fixture("unicornTestSettings").then(
+      ({
+        main_url,
+        shortPasswordErrorMessage,
+        password,
+        newPassword,
+        shortPassword,
+      }) => {
+        cy.log("Settings page");
+        cy.visit(main_url);
+        cy.log("entering current password");
+        cy.get(`[data-test-id=${ELEMENT.currentPassword}]`).type(password);
+        cy.log("entering new password");
+        cy.get(`[data-test-id=${ELEMENT.newPassword}]`).type(newPassword);
+        cy.log("entering repeat new password");
+        cy.get(`[data-test-id=${ELEMENT.repeatNewPassword}]`).type(
+          shortPassword
+        );
+        cy.log("clicking save button");
+        cy.get(`[data-test-id=${ELEMENT.saveChangesButton}]`).click();
+        cy.get(".MuiFormHelperText-root").should(
+          "contain",
+          shortPasswordErrorMessage
+        );
+      }
+    );
+  });
 });
