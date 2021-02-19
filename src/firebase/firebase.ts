020dacf6709373840f79cdb1529184522e849697
Pavel <khokhlovpavel1986@yandex.ru>
2021-02-19 21:23:34 +0300

    Исправлен промис в функции readChatMessages на await

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,7 +3,7 @@ import "firebase/auth";
 import "firebase/storage";
 import "firebase/database";
 import { firebaseConfig } from "./firebaseConfig";
-import { IUser, IChatMessage } from "../interfaces/redux";
+import { IUser /* IChatMessage */ } from "../interfaces/redux";
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
 export const firebaseAuth = firebase.auth();
@@ -21,13 +21,18 @@ export const readUserData = async (uid: string): Promise<IUser> => {
 export const readChatMessagesData = async (
   uid: string,
   callback: (snapshot: firebase.database.DataSnapshot) => void
-): Promise<IChatMessage[]> => {
-  const promise = new Promise((resolve) => {
+): Promise<unknown> => {
+  /* const promise = new Promise((resolve) => {
     const result = db
       .ref("chatMessages/" + uid)
       .limitToLast(10)
       .on("value", callback);
     resolve(result);
   });
-  return promise.then();
+  return promise.then(); */
+  const result = await db
+    .ref("chatMessages/" + uid)
+    .limitToLast(10)
+    .on("value", callback);
+  return result;
 };
