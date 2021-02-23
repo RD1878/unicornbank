960544e8535eb0e3dc37798e105a57aa0d0fe791
Pavel <khokhlovpavel1986@yandex.ru>
2021-02-23 15:03:17 +0300

    Удален эффект у языков

--- src/recoilState/recoilAtoms/languageAtom.ts
+++ src/recoilState/recoilAtoms/languageAtom.ts
@@ -1,4 +1,4 @@
-import { atom, AtomEffect } from "recoil";
+import { atom } from "recoil";
 
 interface ILanguageState {
   language: string;
@@ -6,16 +6,11 @@ interface ILanguageState {
 
 const initialLanguage = "ru";
 
-const languageEffect: AtomEffect<ILanguageState> = ({ onSet }) => {
-  onSet((newLanguage) => newLanguage);
-};
-
 const languageState = atom<ILanguageState>({
   key: "languageState",
   default: {
     language: initialLanguage,
   },
-  effects_UNSTABLE: [languageEffect],
 });
 
 export default languageState;
