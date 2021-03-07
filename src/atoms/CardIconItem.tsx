86b9aadd6ac4de34712a901d741b4a9d6b13a2d1
Pavel <khokhlovpavel1986@yandex.ru>
2021-03-08 00:32:17 +0300

    Изменена иконка карт

--- src/atoms/CardIconItem.tsx
+++ src/atoms/CardIconItem.tsx
@@ -1,11 +1,11 @@
 import { ListItemIcon } from "@material-ui/core";
 import React, { FC } from "react";
-import PaymentRoundedIcon from "@material-ui/icons/PaymentRounded";
+import AccountBalanceWallet from "@material-ui/icons/AccountBalanceWallet";
 
 const CardIconItem: FC = () => {
   return (
     <ListItemIcon>
-      <PaymentRoundedIcon color="secondary" fontSize="large" />
+      <AccountBalanceWallet color="secondary" fontSize="large" />
     </ListItemIcon>
   );
 };
