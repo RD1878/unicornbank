9c6738c4245cad3cc3b9fa3646bc9dac299941ae
Pavel <khokhlovpavel1986@yandex.ru>
2021-03-08 10:49:34 +0300

    Изменен футер

--- src/organisms/Footer.tsx
+++ src/organisms/Footer.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { withTheme } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 import { Logo, PrimaryLink } from "../atoms";
-import { navigation } from "../routes";
+import { ROUTES } from "../routes";
 import { Link } from "react-router-dom";
 import { PHONE_BANK, EMAIL_BANK } from "../constants";
 import { useTranslation } from "react-i18next";
@@ -12,25 +12,17 @@ const Container = withTheme(styled("div")`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 150px;
-  padding: 0px 30px;
+  height: 120x;
+  padding: 20px 30px;
   background-color: ${(props) => props.theme.palette.primary.dark};
 `);
 
-const LeftNavigation = withTheme(styled("div")`
+const MiddleNavigation = withTheme(styled("div")`
   display: flex;
   flex-direction: column;
   align-self: stretch;
   min-width: 50vw;
-  padding: 20px 0;
 `);
-
-const LinksContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-grow: 0.5;
-`;
 
 const RightNavigation = withTheme(styled("div")`
   display: flex;
@@ -49,26 +41,23 @@ const Footer: FC = () => {
 
   return (
     <Container>
-      <LeftNavigation>
+      <Link to={ROUTES.MAIN}>
         <Logo />
-        <LinksContainer>
-          {navigation.map((item) => (
-            <Link to={item.path} key={item.path}>
-              <PrimaryLink component="span">{t(item.name)}</PrimaryLink>
-            </Link>
-          ))}
-        </LinksContainer>
+      </Link>
+      <MiddleNavigation>
+        <Typography color="textSecondary">
+          {t('"Uni Corn Bank" JSC')}
+        </Typography>
         <Typography color="textSecondary">
           {t("Russia, Republic of Tatarstan, Kazan, Pushkin st., 66")}
         </Typography>
-      </LeftNavigation>
-
+        <PrimaryLink href={EMAIL_BANK}>{EMAIL_BANK}</PrimaryLink>
+      </MiddleNavigation>
       <RightNavigation>
         <PrimaryLink href={PHONE_BANK}>{PHONE_BANK}</PrimaryLink>
         <Typography color="textSecondary">
           {t("For calls within Russia")}
         </Typography>
-        <PrimaryLink href={EMAIL_BANK}>{EMAIL_BANK}</PrimaryLink>
       </RightNavigation>
     </Container>
   );
