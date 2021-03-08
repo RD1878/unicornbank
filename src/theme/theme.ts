e31fa8714cebefc1f629ceb9302614792e519e43
Pavel <khokhlovpavel1986@yandex.ru>
2021-03-08 10:20:52 +0300

    Решена проблема с красно-синим цветом в сайдбаре на клике

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -69,6 +69,11 @@ const overrides = {
       fontWeight: 600,
     },
   },
+  MuiTouchRipple: {
+    root: {
+      color: "grey",
+    },
+  },
   MuiOutlinedInput: {
     root: {
       width: "100%",
